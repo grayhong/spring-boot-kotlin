70e9a5c1c4fb65e1d5fbd1f4f011c70fa88a054e
Youngkyu <yk.hong@kaist.ac.kr>
2018-10-21 18:36:20 +0900

    Basic settings: logger, jdbc

--- spring-webflux/src/main/kotlin/com/grayground/webflux/SpringWebfluxApplication.kt
+++ spring-webflux/src/main/kotlin/com/grayground/webflux/SpringWebfluxApplication.kt
@@ -1,10 +1,20 @@
 package com.grayground.webflux
 
+import mu.KLogging
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
-class SpringWebfluxApplication
+class SpringWebfluxApplication: CommandLineRunner {
+
+    companion object: KLogging()
+
+    override fun run(vararg args: String?) {
+        logger.info("hi")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringWebfluxApplication>(*args)
