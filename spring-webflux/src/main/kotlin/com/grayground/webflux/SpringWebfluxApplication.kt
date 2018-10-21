c36abc3fc8a898ec0c0b494bfe2079742f169a4c
Youngkyu <yk.hong@kaist.ac.kr>
2018-10-21 17:28:39 +0900

    Introduce spring-webflux module

---
+++ spring-webflux/src/main/kotlin/com/grayground/webflux/SpringWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.grayground.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxApplication>(*args)
+}
---
+++ spring-webflux/src/test/kotlin/com/grayground/webflux/SpringWebfluxApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.grayground.webflux
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
